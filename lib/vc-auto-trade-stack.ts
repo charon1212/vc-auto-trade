22a18aae47b678658515868b253e55f6b400dec9
charon1212 <charons2turquoise@gmail.com>
2021-05-02 16:26:40 +0900

    AWS CloudWatchをデプロイするコードを追加

--- lib/vc-auto-trade-stack.ts
+++ lib/vc-auto-trade-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as dynamo from '@aws-cdk/aws-dynamodb';
+import * as events from '@aws-cdk/aws-events';
+import * as targets from '@aws-cdk/aws-events-targets';
+
 
 export class VcAutoTradeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -11,6 +14,11 @@ export class VcAutoTradeStack extends cdk.Stack {
       code: lambda.Code.fromAsset('lib/lambda'),
       handler: 'main.handler',
     });
+
+    const rule = new events.Rule(this, 'rule', {
+      schedule: events.Schedule.cron({ minute: '*/5', hour: '*', day: '*', month: '*', year: '*' }),
+    })
+    rule.addTarget(new targets.LambdaFunction(l, {}));
 
   }
 }
