a0db7729fc189197e550dea286100e3c627c937e
charon1212 <charons2turquoise@gmail.com>
2021-05-09 21:37:03 +0900

    バグ修正 - ハンドラー外で定義した値はLambdaコンテナ作成ごとに実行されるだけで、呼び出しごとに実行されるわけではない。

--- lib/lambda/Main/entry.ts
+++ lib/lambda/Main/entry.ts
@@ -2,16 +2,16 @@ import { importProductContextFromDb, saveProductContext } from "./context";
 import saveExecutionHistory from "./ExecutionHistory/saveExecutionHistory";
 import { ProductSetting, productSettings } from "./productSettings";
 
-const now = new Date();
-const before1min = new Date(now.getTime() - 60 * 1000);
-
 export const entry = async () => {
+
+  const now = new Date();
+  const before1min = new Date(now.getTime() - 60 * 1000);
 
   await importProductContextFromDb();
 
   const promiseList: Promise<any>[] = [];
   for (let productSetting of productSettings) {
-    promiseList.push(productEntry(productSetting));
+    promiseList.push(productEntry(productSetting, before1min));
   }
   await Promise.all(promiseList);
 
@@ -21,7 +21,7 @@ export const entry = async () => {
 };
 
 /** プロダクトごとのエントリー */
-const productEntry = async (productSetting: ProductSetting) => {
+const productEntry = async (productSetting: ProductSetting, before1min: Date) => {
 
   await saveExecutionHistory(productSetting.productCode, before1min);
 
