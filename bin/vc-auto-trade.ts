e0c8cd8c9753f0d50b946a6d48095676b42d96c1
charon1212 <charons2turquoise@gmail.com>
2021-05-09 23:00:15 +0900

    余分なログ出力を修正

--- bin/vc-auto-trade.ts
+++ bin/vc-auto-trade.ts
@@ -4,8 +4,6 @@ import * as cdk from '@aws-cdk/core';
 import { VcAutoTradeStack } from '../lib/vc-auto-trade-stack';
 import { VcAutoTradeStackDev } from '../lib/vc-auto-trade-stack-dev';
 
-console.log(process.env.SYSTEM_ENV);
-
 const isProductionBuild = process.env.ENV_NAME === 'production';
 if (isProductionBuild) {
   console.log(`■■■■■■■■■■■■■■■■■■■■
