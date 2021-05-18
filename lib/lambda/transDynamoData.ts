704574dd074cd3b1c3d0785f3fa2e1095f781b78
charon1212 <charons2turquoise@gmail.com>
2021-05-19 07:48:57 +0900

    データ移行バッチを修正。

--- lib/lambda/transDynamoData.ts
+++ lib/lambda/transDynamoData.ts
@@ -3,7 +3,7 @@ import { writeTextFile } from "./Interfaces/AWS/S3/writeTextFile";
 import { productSettings } from "./Main/productSettings";
 
 /**
- * target: 指定した日付を狙い撃ちしてデータ移行する場合は、その年月日を指定する。ただし、2日後の値を指定する必要がある。
+ * target: 指定した日付を狙い撃ちしてデータ移行する場合は、その年月日を指定する。
  * また、monthは0から始まる整数で指定する。それ以外は1から始まる整数で指定する。
  */
 type handlerEvent = {
@@ -12,47 +12,41 @@ type handlerEvent = {
   }
 };
 
-exports.handler = async function (event: handlerEvent) {
+exports.handler = async function (event?: handlerEvent) {
 
-  const now = new Date();
-  const targetDate = event.target;
-
-  const { year, month, date } = targetDate || { year: now.getFullYear(), month: now.getMonth(), date: now.getDate() };
-
+  const targetDate = event?.target ? new Date(event.target.year, event.target.month, event.target.date,) : getYeasterday();
   // 重ければ非同期処理を並列実行させる。今は直列実行（めんどいし、Dailyバッチで速度が必要ないため）
   for (let productSetting of productSettings) {
-    await exec(productSetting.productCode, year, month, date);
+    await exec(productSetting.productCode, targetDate);
   }
-
   return '';
 
 };
 
-/**
- * DynamoDBから実行日付の2日前のデータをすべて取得し、S3にCSV形式で保存した後削除する。
- * @param productCode プロダクトコード。
- * @param year 実行時の年
- * @param month 実行時の月(0スタート)
- * @param day 実行時の日
- */
-const exec = async (productCode: string, year: number, month: number, day: number) => {
+const getYeasterday = () => {
+  const now = new Date();
+  return new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1);
+};
 
-  const before2Day = new Date(year, month, day - 2);
-  const startTimestamp = before2Day.getTime();
+/**
+ * DynamoDBから集計対象日時のデータをすべて取得し、S3にCSV形式で保存する。
+ * @param productCode プロダクトコード。
+ * @param targetDate 集計対象の日時
+ */
+const exec = async (productCode: string, targetDate: Date) => {
+
+  const startTimestamp = targetDate.getTime();
   const endTimestamp = startTimestamp + 24 * 60 * 60 * 1000 - 1;
 
   const res = await searchExecutions(productCode, startTimestamp.toString(), endTimestamp.toString());
 
   if (res?.count && res.count > 0) {
     const csvBody = makeCsvBody(res.result);
-    const yearMonthStr = `${before2Day.getFullYear()}-${before2Day.getMonth() + 1}`;
-    const yearMonthDayStr = yearMonthStr + `-${before2Day.getDate()}`;
+    const yearMonthStr = `${targetDate.getFullYear()}-${targetDate.getMonth() + 1}`;
+    const yearMonthDayStr = yearMonthStr + `-${targetDate.getDate()}`;
     const csvFilePath = `EXEC_HISTORY_${productCode}_${yearMonthStr}/data_${yearMonthDayStr}.csv`;
 
     await writeTextFile(csvFilePath, csvBody);
-
-    // 怖いので、書き込みがうまくいっていることが分かるまでは消さない。
-    // await deleteAllExecution(productCode, res.result);
 
   }
 
