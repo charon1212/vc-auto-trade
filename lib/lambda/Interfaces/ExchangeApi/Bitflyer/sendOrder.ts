b7121101a8648a5386207c245cc0b0b4d7fb8701
charon1212 <charons2turquoise@gmail.com>
2021-05-23 09:47:18 +0900

    リファクタリング

--- lib/lambda/Interfaces/ExchangeApi/Bitflyer/sendOrder.ts
+++ lib/lambda/Interfaces/ExchangeApi/Bitflyer/sendOrder.ts
@@ -1,7 +1,7 @@
 import handleError from "../../../HandleError/handleError";
 import { sendRequest } from "./apiRequest";
 
-export type SendOrderParams = {
+type SendOrderParams = {
   child_order_type: 'LIMIT' | 'MARKET', // 指値注文はLIMIT、成行注文はMARKET
   side: 'BUY' | 'SELL', // 買い注文はBUY、売り注文はSELL
   price?: number, // 価格。child_order_typeがLIMITの場合は必須。
@@ -10,10 +10,16 @@ export type SendOrderParams = {
   time_in_force?: 'GCT' | 'IOC' | 'FOK' // 執行数量条件。省略時はGCT。
 };
 
-export type SendOrderResult = {
-  child_order_acceptance_id: string,
+type SendOrderResult = {
+  child_order_acceptance_id: string, // 注文の受付ID。
 };
 
+/**
+ * 新規注文を出す
+ * @param productCode プロダクトコード。
+ * @param params Bodyパラメータ。詳細は型定義を参照。
+ * @returns エラー時はundefined。受付IDを含むObject。詳細は型定義を参照。
+ */
 export const sendOrder = async (productCode: string, params: SendOrderParams) => {
 
   // 引数チェックを軽く
