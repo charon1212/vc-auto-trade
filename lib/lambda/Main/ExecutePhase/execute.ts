33d40647ecddea2b825e22acb5a449db4260011a
charon1212 <charons2turquoise@gmail.com>
2021-06-13 15:08:28 +0900

    処理フェーズに最終約定IDの更新処理を追加

--- lib/lambda/Main/ExecutePhase/execute.ts
+++ lib/lambda/Main/ExecutePhase/execute.ts
@@ -1,4 +1,7 @@
-import { Order } from "../../Interfaces/DomainType";
+import { Execution, Order } from "../../Interfaces/DomainType";
+import { getProductContext } from "../context";
+import { ProductId } from "../productSettings";
+import { StandardTime } from "../StandardTime";
 import { aggregateExecution } from "./aggregateExecution";
 import { ExecutePhaseFunction } from "./interface";
 import { main } from "./OrderLogic/main";
@@ -6,6 +9,8 @@ import { main } from "./OrderLogic/main";
 export const execute: ExecutePhaseFunction = async (input) => {
 
   const { executions, shortAggregatedExecutions, longAggregatedExecutions, orders, balanceReal, balanceVirtual, productSetting, std } = input;
+
+  await updateLastExecutionId(productSetting.id, executions, std);
 
   const { newShortAggregatedExecutions, newLongAggregatedExecution } =
     await aggregateExecution(executions, shortAggregatedExecutions, longAggregatedExecutions, std);
@@ -27,5 +32,25 @@ export const execute: ExecutePhaseFunction = async (input) => {
     updatedOrder: [...orders, ...newOrders.map((order) => ({ order, }))],
     newLongAggregatedExecution,
   };
+
+};
+
+/**
+ * プロダクトコンテキストを更新する
+ * @param executions 
+ * @param std 
+ */
+const updateLastExecutionId = async (productId: ProductId, executions: Execution[], std: StandardTime) => {
+
+  const lastExecution = executions.find((item) => (item.executionDate.getTime() < std.getStd()));
+  if (lastExecution) {
+    const context = await getProductContext(productId);
+    if (context) {
+      context.lastExecution = {
+        id: lastExecution.id,
+        timestamp: lastExecution.executionDate.getTime(),
+      }
+    }
+  }
 
 };
