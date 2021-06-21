eafaf677c932ffe9e384f5b006f457af8b46419b
charon1212 <charons2turquoise@gmail.com>
2021-06-21 22:32:38 +0900

    バグ修正：関連する平均取引価格を取得できなかった場合の処理を追加

--- lib/lambda/Main/InputPhase/getOrders.ts
+++ lib/lambda/Main/InputPhase/getOrders.ts
@@ -1,4 +1,5 @@
 import { asyncExecution } from "../../Common/util";
+import handleError from "../../HandleError/handleError";
 import { searchDynamoDbStartsWith } from "../../Interfaces/AWS/Dynamodb/db";
 import { dbSettingOrder, getOrderStateCode } from "../../Interfaces/AWS/Dynamodb/dbSettings";
 import { SimpleOrder, OrderState } from "../../Interfaces/DomainType";
@@ -23,8 +24,7 @@ export const getOrders = async (productSetting: ProductSetting,) => {
 
   const orderListFromApi = await getOrdersApi(productSetting, orderListFromDb);
   for (let order of orderListFromDb) {
-    const orderFromApi = orderListFromApi.find((o) => (o.id === order.id));
-    resultOrderList.push({ order: orderFromApi || order, beforeState: order.state }); // API取得があればそれ、なければDB取得のOrder。
+    let orderFromApi = orderListFromApi.find((o) => (o.id === order.id));
     if (productSetting.exchangeCode === 'GMO' && orderFromApi && orderFromApi.state === 'COMPLETED') { // GMOコインの場合、平均取引価格を別途取得する。
       const associatedExecutions = await getAssociatedExecutions(productSetting, orderFromApi);
       let price = 0;
@@ -33,8 +33,14 @@ export const getOrders = async (productSetting: ProductSetting,) => {
         price += exec.price * exec.size;
         size += exec.size;
       }
-      orderFromApi.main.averagePrice = price / size;
+      if (size < 1e-10) {
+        await handleError(__filename, 'getOrders', 'code', '関連する約定を取得できませんでした。注文を完了状態に移行できません。', { productSetting, },);
+        orderFromApi = undefined;
+      } else {
+        orderFromApi.main.averagePrice = price / size;
+      }
     }
+    resultOrderList.push({ order: orderFromApi || order, beforeState: order.state }); // API取得があればそれ、なければDB取得のOrder。
   }
 
   return resultOrderList;
